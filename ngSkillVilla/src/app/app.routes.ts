913de87b1047efe3ab2ca845102d79b702e4d7ba
Michael Ventura <m.ventura14@yahoo.com>
2024-05-23 17:02:39 -0500

    linked routes to register, and login

--- ngSkillVilla/src/app/app.routes.ts
+++ ngSkillVilla/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-
-   // { path: '**', component: NotFoundComponent }
+    
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '**', component: NotFoundComponent }
 ];
--- ngSkillVilla/src/app/components/navigation-bar/navigation-bar.component.ts
+++ ngSkillVilla/src/app/components/navigation-bar/navigation-bar.component.ts
@@ -1,9 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    CommonModule,
+    LoginComponent,
+    RegisterComponent
+  ],
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.css'
 })
