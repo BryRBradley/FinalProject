66581c0f2a07a38897b40edb66ed3347c8a38a3d
Michael Ventura <m.ventura14@yahoo.com>
2024-05-23 17:03:50 -0500

    Home page created and routed

--- ngSkillVilla/src/app/app.routes.ts
+++ ngSkillVilla/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' },
-    
+    { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
     { path: '**', component: NotFoundComponent }
