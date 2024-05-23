319542ee465d657471adbc4ea3a08b38b3e2dd42
Michael Ventura <m.ventura14@yahoo.com>
2024-05-23 18:00:39 -0500

    stubbe out comunity model

--- ngSkillVilla/src/app/models/community.ts
+++ ngSkillVilla/src/app/models/community.ts
@@ -1,2 +1,41 @@
 export class Community {
+
+    id: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+    discordUrl: string;
+    locationId: number;
+    updatedAt: string;
+    createdAt: string;
+    enabled: boolean;
+
+    constructor(
+        id = 0,
+        name = '',
+        description = '',
+        imageUrl = '',
+        discordUrl = '',
+        locationId = 0,
+        updatedAt = '',
+        createdAt = '',
+        enabled = false
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.imageUrl = imageUrl;
+        this.discordUrl = discordUrl;
+        this.locationId = locationId;
+        this.updatedAt = updatedAt;
+        this.createdAt = createdAt;
+        this.enabled = enabled;
+    }
+
+
+
+
+
+
+
 }
