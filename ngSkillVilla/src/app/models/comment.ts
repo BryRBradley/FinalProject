8cf83e979e276f304ed7d6c678aed3e7db78afc7
Michael Ventura <m.ventura14@yahoo.com>
2024-05-23 17:55:14 -0500

    stubbed out comment model

--- ngSkillVilla/src/app/models/comment.ts
+++ ngSkillVilla/src/app/models/comment.ts
@@ -1,2 +1,36 @@
 export class Comment {
+
+    id: number;
+    userId: number;
+    postId: number;
+    inReplyToId: number;
+    message: string;
+    createdAt: string;
+    updatedAt: string;
+
+    constructor(
+        id = 0,
+        postId = 0,
+        userId = 0,
+        inReplyToId = 0,
+        message = '',
+        createdAt = '',
+        updatedAt = ''
+
+    ) {
+        this.id = id;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+        this.message = message;
+        this.userId = userId;
+        this.postId = postId;
+        this.inReplyToId = inReplyToId;
+
+    }
+
+
+
+
+
+
 }
