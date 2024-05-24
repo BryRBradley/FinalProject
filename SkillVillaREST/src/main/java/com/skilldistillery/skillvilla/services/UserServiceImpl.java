b954be2e4dc203338f8a0c98ea0188c5ff9ac979
Mary Hudner <Mhudner@yahoo.com>
2024-05-24 17:56:17 -0600

    commit changes

---
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/services/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.skilldistillery.skillvilla.services;
+
+import java.util.List;
+
+import com.skilldistillery.skillvilla.Repositories.UserRepository;
+import com.skilldistillery.skillvilla.entities.User;
+
+public class UserServiceImpl implements UserService{
+	
+	private UserRepository userRepo;
+	
+	
+
+	public UserServiceImpl(UserRepository userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	@Override
+	public List<User> findAll() {
+		return userRepo.findAll();
+	}
+
+	@Override
+	public User show(int id) {
+		return userRepo.findById(id);
+	}
+
+	@Override
+	public User create(User user) {
+		return userRepo.saveAndFlush(user);
+	}
+
+	@Override
+	public User update(int id, User user) {
+		User updatedUser = userRepo.findById(id);
+		updatedUser.setUsername(user.getUsername());
+		updatedUser.setPassword(user.getPassword());
+		updatedUser.setEmail(user.getEmail());
+		updatedUser.setCreatedAt(user.getCreatedAt());
+		updatedUser.setUpdatedAt(user.getUpdatedAt());
+		updatedUser.setRole(user.getRole());
+		updatedUser.setEnabled(user.isEnabled());
+		return userRepo.saveAndFlush(updatedUser);
+	}
+
+	@Override
+	public void delete(int id) {
+		if(userRepo.existsById(id)) {
+			userRepo.deleteById(id);
+		}
+	}
+	
+	
+	
+	
+
+}
