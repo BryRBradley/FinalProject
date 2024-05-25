9972b227bea7baadc325cf9e244916b7621e61f9
Mary Hudner <Mhudner@yahoo.com>
2024-05-25 11:02:11 -0600

    changed database password

--- SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Repositories/PostRepository.java
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Repositories/PostRepository.java
@@ -1,9 +1,17 @@
 package com.skilldistillery.skillvilla.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.skilldistillery.skillvilla.entities.Post;
+import com.skilldistillery.skillvilla.entities.User;
 
 public interface PostRepository extends JpaRepository<Post, Integer>{
 
+	
+	List <Post> findAll();
+	
+	Post findById(int id);
+	
 }
--- SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/services/PostServiceImpl.java
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/services/PostServiceImpl.java
@@ -1,6 +1,9 @@
 package com.skilldistillery.skillvilla.services;
 
+import java.util.List;
+
 import com.skilldistillery.skillvilla.Repositories.PostRepository;
+import com.skilldistillery.skillvilla.entities.Post;
 
 public class PostServiceImpl implements PostService{
 
@@ -9,6 +12,40 @@ public class PostServiceImpl implements PostService{
 	public PostServiceImpl(PostRepository postRepo) {
 		super();
 		this.postRepo = postRepo;
+	}
+
+	@Override
+	public List<Post> findAll() {
+		return postRepo.findAll();
+	}
+
+	@Override
+	public Post show(int id) {
+		return postRepo.findById(id);
+	}
+
+	@Override
+	public Post create(Post post) {
+		return postRepo.saveAndFlush(post);
+	}
+
+	@Override
+	public Post update(int id, Post post) {
+		Post updatedPost = postRepo.findById(id);
+		updatedPost.setDescription(post.getDescription());
+		updatedPost.setCreatedAt(post.getCreatedAt());
+		updatedPost.setUpdatedAt(post.getUpdatedAt());
+		updatedPost.setEnabled(post.isEnabled());
+		updatedPost.setImageUrl(post.getImageUrl());
+		return postRepo.saveAndFlush(updatedPost);
+	}
+
+	@Override
+	public void delete(int id) {
+		if(postRepo.existsById(id)) {
+			postRepo.deleteById(id);
+		}
+		
 	}
 	
 	
