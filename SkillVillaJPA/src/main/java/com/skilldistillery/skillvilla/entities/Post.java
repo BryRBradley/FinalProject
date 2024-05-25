a01636a85b197acf8524f70186e2105be019d14f
Michael Ventura <m.ventura14@yahoo.com>
2024-05-25 12:40:06 -0500

    added jsonignore on lists of enteties

--- SkillVillaJPA/src/main/java/com/skilldistillery/skillvilla/entities/Post.java
+++ SkillVillaJPA/src/main/java/com/skilldistillery/skillvilla/entities/Post.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -50,6 +52,7 @@ public class Post {
 	@JoinColumn(name="location_id")
 	private Location location;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy="post")
 	private List <Comment> comments;
 	
