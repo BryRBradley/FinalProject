5c09dc3a930f021d198a531c4fa0cfbc6b4b2034
Michael Ventura <m.ventura14@yahoo.com>
2024-05-26 15:15:57 -0500

    .

--- SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Controllers/UserController.java
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.skilldistillery.skillvilla.Controllers;
 
+import java.security.Principal;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.skillvilla.entities.User;
 import com.skilldistillery.skillvilla.services.UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
 @CrossOrigin({ "*", "http://localhost/" })
@@ -20,22 +27,22 @@ public class UserController {
 //	public List<User> findAll(){
 //		return userService.findAll();
 //	}
-//
-//	@GetMapping("users/{id}")
-//	public User show(@PathVariable("id") int id) {
+
+//	@GetMapping("users")
+//	public User show(HttpServletRequest req, HttpServletResponse res, Principal principal) {
 //		return userService.show(id);
 //	}
-//
+
 //	@PostMapping("users")
 //	public User create(@RequestBody User user) {
 //		return userService.create(user);
 //	}
-//	
-//	@PutMapping("users/{id}")
-//	public User update(@PathVariable("id") int id, @RequestBody User user) {
-//		return userService.update(id, user);
-//	}
-//	
+	
+	@PutMapping("users/{id}")
+	public User update(HttpServletRequest req, HttpServletResponse res, Principal principal) {
+		return userService.update(id, user);
+	}
+	
 //	@DeleteMapping("users/{id}")
 //	public void delete(@PathVariable("id") int id) {
 //		userService.delete(id);
