7f2f6095d412f21b1a7b8cc29bd032d5b820437e
Michael Ventura <m.ventura14@yahoo.com>
2024-05-27 11:26:42 -0500

    .

--- SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Controllers/CommentController.java
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Controllers/CommentController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.skillvilla.entities.Comment;
+import com.skilldistillery.skillvilla.entities.Post;
 import com.skilldistillery.skillvilla.services.CommentService;
 import com.skilldistillery.skillvilla.services.PostService;
 
