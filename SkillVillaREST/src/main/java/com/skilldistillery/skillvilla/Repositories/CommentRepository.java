9901f3c43653f08c9f5dcaa48a796da435c65265
Michael Ventura <m.ventura14@yahoo.com>
2024-05-28 14:51:43 -0500

    Feat edit comment endpoint

--- SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Repositories/CommentRepository.java
+++ SkillVillaREST/src/main/java/com/skilldistillery/skillvilla/Repositories/CommentRepository.java
@@ -12,4 +12,6 @@ public interface CommentRepository extends JpaRepository<Comment, Integer>{
 
 	List<Comment> findAllByPostId(int postId);
 
+	boolean existsByIdAndUserUsername(int commentId, String username);
+
 }
